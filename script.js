0695cfda802113b19a0da2c2806818b86e6b4ebb
Julian <juliankl1509@gmail.com>
2024-03-10 13:06:24 +0100

    Serenity and peace

--- script.js
+++ script.js
@@ -29,15 +29,15 @@ setInterval(() => {
 }, 10000);
 
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Check for browser support
-    if ('speechSynthesis' in window) {
-        var msg = new SpeechSynthesisUtterance();
-        var voices = window.speechSynthesis.getVoices();
-        msg.voice = voices.find(voice => voice.gender === 'female' && voice.lang === 'en-US'); // Choose a female voice with US English
-        msg.text = 'Dine analyser gjorde virkelig en forskel'; // Your text to be spoken
-        speechSynthesis.speak(msg);
-    } else {
-        console.log('The browser doesn\'t support the Web Speech API');
-    }
-});
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     // Check for browser support
+//     if ('speechSynthesis' in window) {
+//         var msg = new SpeechSynthesisUtterance();
+//         var voices = window.speechSynthesis.getVoices();
+//         msg.voice = voices.find(voice => voice.gender === 'female' && voice.lang === 'en-US'); // Choose a female voice with US English
+//         msg.text = 'Dine analyser gjorde virkelig en forskel'; // Your text to be spoken
+//         speechSynthesis.speak(msg);
+//     } else {
+//         console.log('The browser doesn\'t support the Web Speech API');
+//     }
+// });
