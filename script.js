62979b817f9b664d0faea06d8e67f29c3934c37b
Julian <juliankl1509@gmail.com>
2024-03-10 12:57:59 +0100

    Serenity and peace

---
+++ script.js
@@ -0,0 +1,29 @@
+const videoSources = [
+    { src: "videos/sunrise.mp4", text: "First Video Message" },
+    // Add more video sources and texts here
+];
+
+let currentVideoIndex = 0;
+
+const videoElement = document.getElementById('video-background');
+const textElement = document.getElementById('video-text');
+
+videoElement.src = videoSources[currentVideoIndex].src;
+textElement.innerHTML = videoSources[currentVideoIndex].text;
+
+videoElement.addEventListener('ended', () => {
+    currentVideoIndex = (currentVideoIndex + 1) % videoSources.length;
+    videoElement.src = videoSources[currentVideoIndex].src;
+    textElement.innerHTML = videoSources[currentVideoIndex].text;
+    videoElement.load();
+    videoElement.play();
+});
+
+// Optionally, switch videos every 10 seconds regardless of video length
+setInterval(() => {
+    currentVideoIndex = (currentVideoIndex + 1) % videoSources.length;
+    videoElement.src = videoSources[currentVideoIndex].src;
+    textElement.innerHTML = videoSources[currentVideoIndex].text;
+    videoElement.load();
+    videoElement.play();
+}, 10000);
